03cd836edb9a126ef59f90afa75ee60e0d77c14a
ki5os <timme.albert.kingma@gmail.com>
2025-05-29 21:54:47 +0200

    new csp

--- webcomponents/unity-stats.js
+++ webcomponents/unity-stats.js
@@ -503,8 +503,8 @@ class UnityStats extends HTMLElement {
                 datasets: [{
                     label: 'Number of Sales',
                     data: values,
-                    backgroundColor: 'rgb(191, 90, 242)',
-                    borderColor: 'rgb(191, 90, 242)',
+                    backgroundColor: 'rgb(0, 118, 255)',
+                    borderColor: 'rgb(0, 118, 255)',
                     borderWidth: 1
                 }]
             },
